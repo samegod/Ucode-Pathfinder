8e5c78ead70658e9816ca644b673b30272ab5c3e
Andrii Zhuk <azhuk@c1r2p1.khpi.ucode-connect.study>
2020-11-18 16:26:44 +0200

    dv

---
+++ src/err_first_line_invalid.c
@@ -0,0 +1,6 @@
+#include "../inc/pathfinder.h"
+
+void err_first_line_invalid() {
+    mx_printstr("error: line 1 is not valid\n");
+    exit(0);
+}
---
+++ src/err_invalid_args.c
@@ -0,0 +1,6 @@
+#include "../inc/pathfinder.h"
+
+void err_invalid_args() {
+    mx_printstr("usage: ./pathfinder [filename]\n");
+    exit(0);
+}
